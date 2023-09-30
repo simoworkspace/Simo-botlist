c127b9b73bf749ca7f727de2cf182c94c45cdb61
Spyei <caio.salamao1234@gmail.com>
2023-09-30 14:28:08 -0300

    feat: Implementing mongobd to queue command

--- src/commands/application-commands/queue.ts
+++ src/commands/application-commands/queue.ts
@@ -1,37 +1,41 @@
-	import type { ApplicationCommandStructure } from "../../typings";
-import axios from "axios";
+import type { ApplicationCommandStructure, BotStructure } from "../../types";
 import { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } from "discord.js";
+import { botSchema } from "../../schemas/Bot";
 
 export default {
-    name: 'queue',
-    async run(client, interaction) {
-//if (interaction.member?.roles.includes("991400149307887696")) return interaction.reply("nao pode");
-			let botsall: Array<{label: string; value: string; description: string}> = [];
-			const req = await axios.get("https://botlist-api.spyei.repl.co/api/bots");
-			const bots = await req.data;
-			const botsverificar = bots.filter((a:any) => !a.approved);
-      const embed = new EmbedBuilder();
-			embed.setTitle("Queue");
-			embed.setColor(0x00ff00);
-			embed.setDescription(botsverificar.map((a:any, index: number) => `**[${index}]** [${a.name}](https://discord.com/api/oauth2/authorize?client_id=${a._id}&permissions=2147483639&scope=bot%20applications.commands)`).join("\n"))
-			botsverificar.map((a:any) => {
-				botsall.push(
-					{
-						label: a.name,
-						description: "oi",
-						value: a._id
-					}
-				)
-			});
-			const actionRow: any = new ActionRowBuilder()
-        .addComponents(
-            new StringSelectMenuBuilder()
-            .setCustomId("selecaobot")
-            .setPlaceholder('Selecione um bot')
-            .setMinValues(1)
-            .setMaxValues(1)
-            .addOptions(botsall)
-        );
-			interaction.reply({ embeds: [embed], components: [actionRow]});
-    },
+	name: "queue",
+	async run(client, interaction) {
+
+		let botsall: Array<{ label: string; value: string; description: string }> = [];
+
+		const bots = await botSchema.find({});
+		const botsverificar = bots.filter((a: BotStructure) => !a.approved);
+
+		const embed = new EmbedBuilder()
+			.setTitle("Queue")
+			.setColor(0x00ff00)
+			.setDescription(botsverificar.map((a: any, index: number) => `**[${index}]** [${a.name}](https://discord.com/api/oauth2/authorize?client_id=${a._id}&permissions=2147483639&scope=bot%20applications.commands)`).join("\n"))
+
+		botsverificar.map((a: BotStructure) => {
+			botsall.push(
+				{
+					label: a.name,
+					description: a.short_description.slice(0, 50) + "...",
+					value: a._id
+				}
+			)
+		});
+
+		const actionRow: any = new ActionRowBuilder()
+			.addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId("selecaobot")
+					.setPlaceholder('Selecione um bot')
+					.setMinValues(1)
+					.setMaxValues(1)
+					.addOptions(botsall)
+			);
+
+		interaction.reply({ embeds: [embed], components: [actionRow] });
+	},
 } as ApplicationCommandStructure;
