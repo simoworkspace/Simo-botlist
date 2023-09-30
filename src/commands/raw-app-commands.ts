f8edb5d12dcbfba41caa9725b4f1f556bd2e3f8c
Spyei <caio.salamao1234@gmail.com>
2023-09-30 15:03:53 -0300

    feat: Adding decode command to raw

--- src/commands/raw-app-commands.ts
+++ src/commands/raw-app-commands.ts
@@ -2,28 +2,41 @@ import { ApplicationCommandData, ApplicationCommandType, ApplicationCommandOptio
 
 export const JSONStructure: ApplicationCommandData[] = [
     {
-        name: 'ping',
+        name: "ping",
         type: ApplicationCommandType.ChatInput,
-        description: 'Veja como o Simo está operando',
+        description: "Veja como o Simo está operando",
         options: [
             {
-                name: 'shard',
-                description: 'Veja a latência de um fragmento específico',
+                name: "shard",
+                description: "Veja a latência de um fragmento específico",
                 type: ApplicationCommandOptionType.Integer,
                 min_value: 0,
                 required: false
             }
         ]
     },
-	{
-		    
-        name: 'addbot',
+    {
+
+        name: "addbot",
         type: ApplicationCommandType.ChatInput,
-        description: 'Adicione seu bot na botlist',
-	},
-	{
-		name: 'queue',
-		type: ApplicationCommandType.ChatInput,
-        description: 'Veja os bots pendentes para serem analisados',
-	}
+        description: "Adicione seu bot na botlist",
+    },
+    {
+        name: "queue",
+        type: ApplicationCommandType.ChatInput,
+        description: "Veja os bots pendentes para serem analisados",
+    },
+    {
+        name: "decode",
+        type: ApplicationCommandType.ChatInput,
+        description: "Descriptografique seu token recebido pelo site",
+        options: [
+            {
+                name: "token",
+                description: "Insira o jwt abaixo",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ]
+    }
 ];
