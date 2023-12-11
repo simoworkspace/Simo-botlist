60c51f86d38c56699a97011ab53f45795cb3c534
Spyei <caio.salamao1234@gmail.com>
2023-12-11 17:11:14 -0300

    feat: banner_url in userinfo

--- src/commands/application-commands/user.ts
+++ src/commands/application-commands/user.ts
@@ -1,37 +1,43 @@
-import type { ApplicationCommandStructure, Team } from "../../types";
+import type { ApplicationCommandStructure } from "../../types";
 import { userSchema } from "../../schemas/User";
 import { botSchema } from "../../schemas/Bot";
 import { EmbedBuilder } from "discord.js";
 
 export default {
-    name: "user-info",
-    async run(client, interaction) {
-      const user = interaction.options.getUser("user") || interaction.user;
-			if(!user) return;
-			const userDb = await userSchema.findById(user.id);
-      const userBots = await botSchema.find({
-				owner_id: user.id
-			});
-			const userTeams = (await userSchema.find({})).filter((usero) => usero.team.members?.some((member) => member.id === user.id));
-			const embed = new EmbedBuilder()
+	name: "user-info",
+	async run(client, interaction) {
+		const user = interaction.options.getUser("user") || interaction.user;
+		const userDb = await userSchema.findById(user.id);
+
+		if (!userDb) return interaction.reply("Usuário não encontrado no banco de dados.");
+
+		const userBots = await botSchema.find({
+			owner_id: user.id
+		});
+
+		const embed = new EmbedBuilder()
 			.setColor(0x0000ff)
 			.setTitle(":middle_finger: Informações do usuário")
 			.setThumbnail(user.displayAvatarURL())
 			.addFields({
 				name: "Usuário",
-				value: `**${user.username}** (\`${user.id}\`)`,
+				value: `**${userDb.username}** (\`${userDb._id}\`)`,
 			}, {
 				name: "Biografia",
 				value: userDb?.bio ? userDb.bio : "Nenhuma biografia definida",
 			}, {
 				name: "Bots",
 				value: userBots.length ? userBots.map((bot) => `[${bot.name}](https://bombadeagua.life/bot/${bot._id})`).join(", ") : "Nenhum bot adicionado",
-			}, {
-			  name: "Times",
-				value: userTeams.length !== 0 ? userTeams.map((team: any) => `[${team.team.name}](https://bombadeagua.life/time/${team.team_id}), ${team.team.members?.length} Membros`).join("\n") : `${user.username} não possuí times.`,
 			});
-			return interaction.reply({
-				embeds: [embed]
-			})
-    }
+
+		console.log("banner_url" in userDb);
+
+		if (userDb?.banner_url) {
+			embed.setImage(userDb.banner_url);
+		}
+
+		return interaction.reply({
+			embeds: [embed]
+		});
+	}
 } as ApplicationCommandStructure;
