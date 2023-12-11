4dabe139368ba568000a4eb1e8ad4880935c8ccc
Spyei <caio.salamao1234@gmail.com>
2023-12-11 16:42:50 -0300

    fix: queue types

--- src/commands/application-commands/queue.ts
+++ src/commands/application-commands/queue.ts
@@ -1,36 +1,37 @@
 import type { ApplicationCommandStructure, BotStructure } from "../../types";
-import { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuInteraction, ButtonBuilder, ButtonStyle, ButtonInteraction, IntegrationApplication } from "discord.js";
+import { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuInteraction, ButtonBuilder, ButtonStyle } from "discord.js";
 import { botSchema } from "../../schemas/Bot";
 import { userSchema } from "../../schemas/User";
-import type { UserStructure } from "../../types";
 
 export default {
 	name: "queue",
-	async run(client, interaction: any) {
-		if (!interaction.member?.roles.cache.has("991400149307887696")) return interaction.reply("Você precisa ser um verificador para usar o comando.");
-		
+	async run(client, interaction) {
+		const members = await interaction.guild?.members.fetch();
+
+		if (!members?.find((member) => member.id === interaction.user.id)?.roles.cache.has("991400149307887696")) return interaction.reply("Você precisa ser um verificador para usar o comando.");
+
 		let botsall: Array<{ label: string; value: string; description: string }> = [];
 
-		const bots: BotStructure[] = await botSchema.find({ approved: false });
+		const bots = await botSchema.find({ approved: false });
 
 		if (bots.length === 0) return interaction.reply({ content: "Não há bots para serem verificados." });
 
 		const embed: EmbedBuilder = new EmbedBuilder()
 			.setTitle("Queue")
 			.setColor(0x00ff00)
-			.setDescription(bots.map((a: any, index: number) => `**[${index + 1}]** [${a.name}](https://discord.com/api/oauth2/authorize?client_id=${a._id}&scope=bot%20applications.commands)`).join("\n"))
+			.setDescription(bots.map((a, index) => `**[${index + 1}]** [${a.name}](https://discord.com/api/oauth2/authorize?client_id=${a._id}&scope=bot%20applications.commands)`).join("\n"))
 
-		bots.map((a: BotStructure) => {
+		bots.map((bot) => {
 			botsall.push(
 				{
-					label: a.name,
-					description: a.short_description.slice(0, 50) + "...",
-					value: a._id
+					label: bot.name,
+					description: bot.short_description.slice(0, 50) + "...",
+					value: bot._id
 				}
 			)
 		});
 
-		const actionRow: ActionRowBuilder<any> = new ActionRowBuilder()
+		const actionRow = new ActionRowBuilder<StringSelectMenuBuilder>()
 			.addComponents(
 				new StringSelectMenuBuilder()
 					.setCustomId("selecaobot")
@@ -40,9 +41,12 @@ export default {
 					.addOptions(botsall)
 			);
 
-		const int = await interaction.reply({ embeds: [embed], components: [actionRow] });
+		const int = await interaction.reply({
+			embeds: [embed],
+			components: [actionRow],
+		});
 
-		const colector = int.createMessageComponentCollector({ filter: (i: any) => i.user.id === interaction.user.id, time: 30000 });
+		const colector = int.createMessageComponentCollector({ filter: (i) => i.user.id === interaction.user.id, time: 30000 });
 
 		colector.on("collect", async (interaction: StringSelectMenuInteraction) => {
 			if (!interaction.isStringSelectMenu()) return;
@@ -55,7 +59,7 @@ export default {
 				.setColor(0x00ff00)
 				.setDescription(`Você deseja aprovar o bot **${selbot?.name}**, ou recusar? Escolha uma das alternativas abaixo.`);
 
-			const actionRow: ActionRowBuilder<any> = new ActionRowBuilder()
+			const actionRow = new ActionRowBuilder<ButtonBuilder>()
 				.addComponents(
 					new ButtonBuilder()
 						.setCustomId("aprovado")
@@ -71,9 +75,9 @@ export default {
 
 			const int = await interaction.update({ embeds: [embed], components: [actionRow] });
 
-			const colector = int.createMessageComponentCollector({ filter: (i) => i.user.id === interaction.user.id, time: 30000 });
+			const colector = int.createMessageComponentCollector({ filter: i => i.user.id === interaction.user.id, time: 30000 });
 
-			colector.on("collect", async (interaction: any) => {
+			colector.on("collect", async (interaction) => {
 				if (!interaction.isButton()) return;
 
 				if (interaction.customId === "aprovado") {
@@ -81,51 +85,62 @@ export default {
 						approved: true
 					})
 
-					interaction.guild.members.cache.get(selbot?._id).roles.add("988515244601118810");
+					const selbotMember = interaction.guild?.members.cache.get(selbot?._id);
 
-					selbot?.owners.forEach(owner => interaction.guild.members.cache.get(owner).roles.add("991507628553412759"));
+					if (selbotMember) {
+						selbotMember.roles.add("988515244601118810");
+					}
+
+					const ownerMember = interaction.guild?.members.cache.get(selbot?.owner_id);
+
+					if (ownerMember) {
+						ownerMember.roles.add("991507628553412759");
+					}
+
 
 					await fetch(process.env.WEBHOOK as string, {
 						headers: {
-							"Content-Type":  'application/json'
+							"Content-Type": 'application/json'
 						},
 						method: "POST",
 						body: JSON.stringify({
-							content: selbot?.owners.length === 0 ? `<@${selbot?.owners[0]}>` : selbot?.owners.map(a => `<@${a}>`).join(", ") + `\n Seu bot: **${selbot?.name}** (\`${selbot?._id}\`) foi aprovado!`,
+							content: `<@${selbot?.owner_id}> :white_check_mark: **|** Seu bot: **${selbot?.name}** (\`${selbot?._id}\`) foi aprovado!`,
 						})
 					});
 
-					await interaction.update({ content: `Bot **${selbot?.name}** foi aprovado com sucesso!`, embeds: [], components: [] })
+					return void interaction.update({ content: `Bot **${selbot?.name}** foi aprovado com sucesso!`, embeds: [], components: [] })
 				} else {
 					await botSchema.findByIdAndDelete(selbot?._id);
 
 					await fetch(process.env.WEBHOOK as string, {
 						headers: {
-							"Content-Type":  'application/json'
+							"Content-Type": 'application/json'
 						},
 						method: "POST",
 						body: JSON.stringify({
-							content: selbot?.owners.length === 0 ? `<@${selbot?.owners[0]}>` : selbot?.owners.map(a => `<@${a}>`).join(", ") + `\n :x: **|** Seu bot: **${selbot?.name}** (\`${selbot?._id}\`) foi recusado.`,
+							content: `<@${selbot?.owner_id}>\n :x: **|** Seu bot: **${selbot?.name}** (\`${selbot?._id}\`) foi recusado.`,
 						})
 					});
-const user: null | any = await userSchema.findById(selbot?.owners[0]);
-					
-const notificationsId = [...user.notifications.keys()];
+					const user = await userSchema.findById(selbot?.owner_id);
 
-    user.notifications.set(
-        notificationsId.length < 1
-            ? "1"
-            : `${Math.max(...notificationsId.map(Number)) + 1}`,
-        {
-					  content: `Seu bot **${selbot?.name}** foi recusado.`,
-					  type: 2,
-            sent_at: new Date().toISOString(),
-				}
-			
-    );
+					if (!user) return void interaction.update("Esse dono não está registrado na db");
 
-          await user.save();
-					await interaction.update({ content: `Bot **${selbot?.name}** foi recusado com sucesso!`, embeds: [], components: [] })
+					const notificationsId = [...user.notifications.keys()];
+
+					user.notifications.set(
+						notificationsId.length < 1
+							? "1"
+							: `${Math.max(...notificationsId.map(Number)) + 1}`,
+						{
+							content: `Seu bot **${selbot?.name}** foi recusado.`,
+							type: 2,
+							sent_at: new Date().toISOString(),
+						}
+
+					);
+
+					await user.save();
+					return void interaction.update({ content: `Bot **${selbot?.name}** foi recusado com sucesso!`, embeds: [], components: [] })
 				}
 			});
 		});
